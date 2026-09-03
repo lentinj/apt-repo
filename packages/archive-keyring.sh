94d427251e19f2fce1c0e5c4c91df0868370b71b
Jamie Lentin <jm@lentin.co.uk>
2026-09-03 18:10:36 +0000

    archive-keyring.sh

--- packages/archive-keyring.sh
+++ packages/archive-keyring.sh
@@ -8,14 +8,13 @@ PROJECT="$(awk '/^Origin:/ { print $2 }' "${REPO_DIR}/conf/distributions")"
 DEB_DIR="${WORK_DIR}/${PROJECT}-archive-keyring"
 
 mkdir -p "${DEB_DIR}/DEBIAN"
-# TODO: Flag file to get the author from git?
 cat > "${DEB_DIR}/DEBIAN/control" <<EOF
 Package: $PROJECT-archive-keyring
-Version: 1.0.0-1
+Version: 1
 Section: misc
 Priority: optional
 Architecture: all
-Maintainer: $(git log --pretty=format:'%an <%ae>' | head -1)
+Maintainer: ${KEY_AUTHOR}
 Description: OpenPGP archive certificates of $PROJECT
  $PROJECT digitally signs its Release files. This package
  contains the archive certificates used for that.
@@ -29,8 +28,12 @@ gpg --export-options export-minimal --export "${KEY_AUTHOR}" \
 mkdir -p "${DEB_DIR}/etc/apt/sources.list.d"
 
 COMPONENTS="$(awk '/^Components:/ {  sub("^Components: *", "") ; print }' "${REPO_DIR}/conf/distributions" | head -1)"
-cat >> "${DEB_DIR}/etc/apt/sources.list.d/${PROJECT}.list" <<EOF
-deb [signed-by=/usr/share/keyrings/$PROJECT.pgp] ${PUBLISH_URL} ${PROJECT_REL} ${COMPONENTS}
+cat >> "${DEB_DIR}/etc/apt/sources.list.d/${PROJECT}.sources" <<EOF
+Types: deb
+URIs: ${PUBLISH_URL}
+Suites: ${PROJECT_REL}
+Components: ${COMPONENTS}
+Signed-By: signed-by=/usr/share/keyrings/$PROJECT.pgp
 EOF
 
 dpkg-deb --root-owner-group --build "${WORK_DIR}/${PROJECT}-archive-keyring"
