1e7a45f85128cb93f1e339b86dd97c188884f22d
Jamie Lentin <jm@lentin.co.uk>
2026-09-05 13:56:20 +0000

    Use --sign-keyid

--- quickpkg.py
+++ quickpkg.py
@@ -43,9 +43,12 @@ def cfg_to_package(cfg_path, base="/tmp/quickpkg", distribution="stable"):
             os.chmod(os.path.join(package_path, source_path), chmod_mode)
             dh_install_f.write("%s %s\n" % (source_path, os.path.dirname(dest_path)))
 
-    cmd = ["/usr/bin/dpkg-buildpackage", "-uc"]  # NB: Changelog will have git author, not KEY_AUTHOR
+    cmd = ["/usr/bin/dpkg-buildpackage"]
     if not os.environ.get("KEY_AUTHOR"):
-        cmd.extend(["-us"])
+        cmd.extend(["-us", "-uc"])
+    else:
+        # NB: Changelog will have git author, not KEY_AUTHOR
+        cmd.extend(["--sign-keyid=" + os.environ["KEY_AUTHOR"]])
     subprocess.run(cmd, cwd=package_path, check=True)
 
 
